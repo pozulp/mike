5c06d245801e4ca29cc300e4d16cc74124967b89
mmpozulp <mmpozulp@email.wm.edu>
2015-01-01 02:21:56 -0500

    added review support

--- scripts/book_reader.py
+++ scripts/book_reader.py
@@ -3,45 +3,131 @@
 # Reads the books.template file and replaces the 
 # all-capitals placeholders with the data from
 # the books.txt file and a modal if there is 
-# a corresponding review for this book
+# a corresponding review for this book which
+# can be found in reviews.txt
+
+''' books.txt file format
+Header                  - The first line gets turned into a top level accordion tab
+Reviewed, Title, Author - Subsequent lines are interpreted as CSV
+Reviewed, Title, Author - Reviewed is boolean, "N" or "Y" if book was reviewed
+Reviewed, Title, Author - Title is the title of the book
+Reviewed, Title, Author - Author is the author of the book
+                        - An empty line indicating the end of this accordion tab 
+(Repeat as for all accordion tabs)
+'''
+
+''' reviews.txt file format
+Title, Author           - The title and the author of the book being reviewed
+RTitle                  - The title of the review
+Review                  - All of the text of the review
+                        - An empty line indicating the end of this review
+(Repeat for all reviews)
+'''
 
 import sys
 
 BOOK_LIST = 'books.txt'
+BOOK_REVIEWS = 'reviews.txt'
 TEMPLATE = 'templates/books.template'
 
-# Used to encase each book title/author
-OPEN = '<a href="#" class="list-group-item text-center" data-toggle="modal" data-target="#ofMiceAndMen">'
-CLOSE = '</a>'
+# For books with a review, use modal 
+HAS_REVIEW_OPEN = '<a href="#" class="list-group-item text-center" data-toggle="modal" data-target="#{}">'
+HAS_REVIEW_CLOSE = '</a>'
+
+# For books without a review, don't use modal
+NO_REVIEW_OPEN = '<div class="list-group-item text-center">'
+NO_REVIEW_CLOSE = '</div>'
 
 # Used to offset the author with smaller text
 SM_OPEN = '<small>'
 SM_CLOSE = '</small>'
 
 # Finds heading in template and replaces it with the sublist
-def replace(heading, sublist, template):
+# Retuns a list of books that have reviews 
+def replace_accordion(heading, sublist, template):
 
     # Build the string from the sublist that we want to use
     # to replace the heading in the template
     strlist = ''
-    for pair in sublist:
-        book, author = tuple(pair.split(", "))
-        strlist += OPEN + book + " " + SM_OPEN + author + SM_CLOSE + CLOSE + '\n'
 
+    for item in sublist:
+
+        read, title, author = tuple(item.split(", "))
+
+        # If the book has no review, don't link to a modal
+        if read == 'N':
+            strlist += NO_REVIEW_OPEN
+        else:
+            # Strip strings of whitespace to match modal
+            # for use as modal identifiers, for example
+            # Of Mice And Men -----> OfMiceAndMen
+            title_target = "".join(title.split())
+            strlist += HAS_REVIEW_OPEN.format(title_target)
+
+        strlist += title + " " + SM_OPEN + author + SM_CLOSE 
+
+        # If the book has no review, don't link to a modal
+        if read == 'N':
+            strlist += NO_REVIEW_CLOSE
+        else:
+            strlist += HAS_REVIEW_CLOSE
 
     # Replace the heading in the template with our list of book/auth elements
     i = template.index(heading)
     template[i] = strlist
 
+# Go through the list of books that have reviews
+# and extract their reviews from reviews.txt
+# and place them in their modals
+def replace_reviews(template):
 
-def make_books():
+    with open(BOOK_REVIEWS) as f:
+        reviews = [x.strip() for x in f.readlines()]
+
+    # Substitute each review into its appropriate place in the modal 
+    while len(reviews) > 0:
+
+        book_title, author = tuple(reviews[0].split(", "))
+
+        review_title = reviews[1]
+
+        # Get the review body which is of arbitrary length
+        i = 2
+        body = ""
+        while i < len(reviews) and reviews[i] != '':
+            body += reviews[i]
+            i += 1
+
+        # Update reviews so on the next iteration we
+        # are looking at the next book review entry
+        reviews = reviews[i+1:]
+
+        # The key into the template is the 
+        # full capitalizion of the 
+        # book title, for example
+        # Of Mice and Men -----> OF MICE AND MEN
+        key = book_title.upper()
+
+        # Replace the modal title with the review title
+        i = template.index(key)
+        template[i] = review_title
+
+        # Replace the modal body with the review content
+        i = template.index(key)
+        template[i] = body
+
+        # Replace the modal footer with the title, author
+        i = template.index(key)
+        template[i] = book_title + " " + SM_OPEN + author + SM_CLOSE
+
+
+def make_book_page():
 
     # Read in the book list and the template files
     with open(BOOK_LIST) as f:
         book_list = [x.strip() for x in f.readlines()]
     with open(TEMPLATE) as f:
         template = [x.strip() for x in f.readlines()]
-
 
     # Find each heading in the book list and its associated books
     # and substitute them into the template
@@ -55,9 +141,12 @@ def make_books():
             i += 1
 
         sublist = book_list[1:i]
-        replace(heading, sublist, template)
+        replace_accordion(heading, sublist, template)
 
         book_list = book_list[i+1:]
+
+    # Add the book review text into their modals
+    replace_reviews(template)
 
     # Add the newlines back to the template file
     template = [x + '\n' for x in template]
@@ -68,4 +157,4 @@ def make_books():
     fo.close()
 
 if __name__ == '__main__':
-    make_books()
+    make_book_page()
