6365fa4e590d8066869a7af6cb6387d1dcfe43e6
mmpozulp <mmpozulp@email.wm.edu>
2015-07-12 23:32:21 -0700

    added recipes, removed home link, and wrote script to generate recipes page

--- scripts/recipe_page_maker.py
+++ scripts/recipe_page_maker.py
@@ -7,19 +7,35 @@ from os import listdir
 
 # Make sure to run this script from the base directory
 # /Users/Mike/workspace/mike so the relative path below works
-PATH = "recipes"
 
-# Write out to the index.html file of the recipes directory
-OUT_FILE = PATH + "/index.html"
+RECIPE_PATH = "recipes"
+TEMPLATE = "templates/recipes.template"
+OUT_FILE = "recipes/recipes.gen.html"
+START_FLAG = "RECIPES"
 
 # Each line of the file is a link to the recipe
-LINK_LINE = '<a href="{}"> {} </a><br>'
+LINK_LINE = '<li class="recipe"> <a href="{}"> {} </a> </li>'
 
-# Open the file we are writing to, then make the link html
+# Build the links using the .rtf resource names in the /recipes/
+# directory since the .rtf extension indicates a recipe
+recipes = listdir(RECIPE_PATH)
+links = [LINK_LINE.format(recipe, recipe) for recipe in recipes if "rtf" in recipe]
+
+# Open and read the template file
+with open(TEMPLATE) as f:
+    template = [x.strip() for x in f.readlines()]
+
+# Replace the recipe flag in the template with our recipe content
+i = template.index(START_FLAG)
+template.pop(i)
+template[i:i] = links
+
+for x in template:
+    print x
+# Add the newlines back to the template file
+template = [x + '\n' for x in template]
+
+# Open and write to the output file
 fo = open(OUT_FILE, "w+")
-recipes = listdir(PATH)
-links = [LINK_LINE.format(recipe, recipe) + '\n' for recipe in recipes]
-
-# Write to and close the file
-fo.writelines(links)
+fo.writelines(template)
 fo.close()
