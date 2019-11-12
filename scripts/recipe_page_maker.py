b2c311b850226f97c50b822786b37a0b22c31b22
Mike Pozulp <pozulp.llvm@gmail.com>
2019-11-11 20:46:12 -0800

    Add Amandas roasted veg recipe

--- scripts/recipe_page_maker.py
+++ scripts/recipe_page_maker.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # recipe_page_maker.py
 # Make the index.html page containing
 # the list of recipes
@@ -19,7 +19,8 @@ LINK_LINE = '<li class="recipe"> <a href="{}"> {} </a> </li>'
 # Build the links using the .rtf resource names in the /recipes/
 # directory since the .rtf extension indicates a recipe
 recipes = listdir(RECIPE_PATH)
-links = [LINK_LINE.format(recipe, recipe) for recipe in recipes if "rtf" in recipe]
+links = sorted([LINK_LINE.format(x, x) for x in recipes
+                if x.endswith('rtf') or x.endswith('txt')])
 
 # Open and read the template file
 with open(TEMPLATE) as f:
@@ -31,7 +32,7 @@ template.pop(i)
 template[i:i] = links
 
 for x in template:
-    print x
+    print(x)
 # Add the newlines back to the template file
 template = [x + '\n' for x in template]
 
